f683e1bfe2defe2caf829a62b15e8aef8ccec55e
unknown <sarthakdhoke132@gmail.com>
2024-09-23 22:38:24 +0530

    done animation on hover image show

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+
+// for diplay block and none
+var  elemContainer = document.querySelector("#hover-container");
+var  fixed = document.querySelector("#fixed-image");
+
+elemContainer.addEventListener("mouseenter", function(){
+    fixed.style.display = "block";
+})
+
+elemContainer.addEventListener("mouseleave", function(){
+    fixed.style.display = "none";
+})
+
+
+
+// for image diplay for each hover 
+
+var hover_Eff = document.querySelectorAll(".hover-effect");
+
+hover_Eff.forEach(function(image){
+    image.addEventListener("mouseenter", function(){
+        var img = image.getAttribute("data-image");
+        fixed.style.backgroundImage = `url(${img})`;
+    })
+})
